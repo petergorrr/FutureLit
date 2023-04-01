61e93a05220d760cb54f49cd9d6c86e91bc8fdf5
Chai1237 <90553225+Chai1237@users.noreply.github.com>
2023-04-01 21:01:14 +0800

    Tab 2

--- futurelit/src/app/tab2/tab2.page.ts
+++ futurelit/src/app/tab2/tab2.page.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, CommonModule, FormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Tab2Page {
 
   constructor() {}
 
+  defaultView = 'explore'; // default view
 }
