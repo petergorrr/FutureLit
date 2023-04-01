e8afdab2f167c7b09cd2e0f79b9d448a1b4f14c2
Moses Lau Yi Hieng <65391344+heiguy123@users.noreply.github.com>
2023-04-01 17:55:51 +0800

    2nd Ver of Skill Set Page

--- futurelit/src/app/skill-set/skill-set.page.ts
+++ futurelit/src/app/skill-set/skill-set.page.ts
@@ -2,13 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-skill-set',
   templateUrl: './skill-set.page.html',
   styleUrls: ['./skill-set.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SkillSetPage {
 
