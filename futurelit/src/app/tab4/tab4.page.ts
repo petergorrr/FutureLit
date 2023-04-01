1fc16363f2bb573104466fdd3a98d88ba9b4a1e1
Moses Lau Yi Hieng <65391344+heiguy123@users.noreply.github.com>
2023-04-01 16:03:25 +0800

    Profile Page

--- futurelit/src/app/tab4/tab4.page.ts
+++ futurelit/src/app/tab4/tab4.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
@@ -10,5 +11,9 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   imports: [IonicModule, ExploreContainerComponent],
 })
 export class Tab4Page {
-  constructor() {}
+  constructor(private router: Router) { }
+  
+  goToPage(route: string) {
+    this.router.navigate([route]);
+  }
 }
